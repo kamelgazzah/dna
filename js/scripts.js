2082fb93356de37f24511afe002a310d3fce5a98
kamelgazzah <kamelgazzah@gmail.com>
2025-07-15 23:28:23 +0100

    update link

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 const menu = () => {
-    fetch('/menu/menu.html')
+    fetch('menu/menu.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('menu').innerHTML = data;
